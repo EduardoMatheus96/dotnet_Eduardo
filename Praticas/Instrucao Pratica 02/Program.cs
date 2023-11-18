3779c0a98530c8e0615817f16f1029706ab30b83
Eduardo Matheus <eduardomatheusuesc@gmail.com>
2023-11-18 16:02:21 -0300

    Create Method of Remove from list of tasks

--- Praticas/Instrucao Pratica 02/Program.cs
+++ Praticas/Instrucao Pratica 02/Program.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-List<string> createTask(string infoTask, string check)
+List<dynamic> createTask(string titleTask, string infoTask, string check, DateTime date)
 {
-    List<string> task = new List<string>();
+    List<dynamic> task = new List<dynamic>();
+    task.Add(titleTask);
     task.Add(infoTask);
     task.Add(check);
+    // tratamento da data ...
+    task.Add(date);
     return task;
 }
 
-List<List<string>> ListOfTasks(List<string> task)
+
+List<List<dynamic>> ListOfTasks(List<dynamic> task)
 {
-    List<List<string>> listTasks = new List<List<string>>();
+    List<List<dynamic>> listTasks = new List<List<dynamic>>();
     listTasks.Add(task);
     return listTasks;
 }
 
-void viewListOfTasks(List<List<string>> listTasks)
+void viewListOfTasks(List<List<dynamic>> listTasks)
 {
-    var enumerateTasks = listTasks.Select((listTasks, index) => $"{index + 1}. {listTasks[0]} - {listTasks[1]}");
+    var enumerateTasks = listTasks.Select((listTasks, index) => $"{index + 1}. {listTasks[0]} - {listTasks[1]} - {listTasks[2]} - {listTasks[3]}");
     foreach (var item in enumerateTasks)
     {
         Console.WriteLine(item);
@@ -26,54 +30,56 @@ void viewListOfTasks(List<List<string>> listTasks)
 
 }
 
-static void changeTask(List<List<string>> listTasks, int index)
+void changeTask(List<List<dynamic>> listTasks, int index)
 {
-    int choice;
-    Console.WriteLine("Digite 1 para alterar o texto e 0 para marcar como concluida a tarefa: ");
-    choice = Convert.ToInt32(Console.ReadLine());
-    if (choice == 1)
+    if (index >= 1 && index <= listTasks.Count)
     {
-        if (index >= 1 && index <= listTasks.Count)
-        {
-            Console.WriteLine($"Tarefa atual: {listTasks[index - 1][0]}");
+        Console.WriteLine($"Tarefa atual: {listTasks[index - 1][0]}");
 
-            Console.WriteLine("Digite o novo texto da tarefa: ");
-            string novoTexto = Console.ReadLine();
+        // Modificando a tarefa na lista
+        listTasks[index - 1][1] = "[X]";
 
-            // Modificando a tarefa na lista
-            listTasks[index - 1][0] = novoTexto;
-
-            Console.WriteLine("Tarefa modificada com sucesso!");
-        }
-        else
-        {
-            Console.WriteLine("Índice inválido. Tarefa não encontrada.");
-        }
-    } else {
-        if (index >= 1 && index <= listTasks.Count)
-        {
-            Console.WriteLine($"Tarefa atual: {listTasks[index - 1][0]}");
-
-            // Modificando a tarefa na lista
-            listTasks[index - 1][1] = "[X]";
-
-            Console.WriteLine("Tarefa modificada com sucesso!");
-        }
-        else
-        {
-            Console.WriteLine("Índice inválido. Tarefa não encontrada.");
-        }
+        Console.WriteLine("Tarefa modificada com sucesso!");
+    }
+    else
+    {
+        Console.WriteLine("Índice inválido. Tarefa não encontrada.");
     }
 }
 
-string taskText, _switch;
-List<string> task;
-List<List<string>> listTasks = new List<List<string>>();
+void removeTaskOfList(List<List<dynamic>> listTasks, int index){
+    if (index >= 1 && index <= listTasks.Count)
+    {
+        Console.WriteLine($"Tarefa atual: {listTasks[index - 1][0]}");
+
+        // Modificando a tarefa na lista
+        listTasks.RemoveAt(index - 1);
+
+        Console.WriteLine("Tarefa removida com sucesso!");
+    }
+    else
+    {
+        Console.WriteLine("Índice inválido. Tarefa não encontrada.");
+    }
+}
+
+
+string taskText, _switch, titleTask, inputDate;
+DateTime date;
+List<dynamic> task;
+List<List<dynamic>> listTasks = new List<List<dynamic>>();
 do
 {
     Console.WriteLine("Insira uma nova tarefa: ");
+    Console.Write("Titulo: ");
+    titleTask = Console.ReadLine();
+    Console.Write("Descrição: ");
     taskText = Console.ReadLine();
-    task = createTask(taskText, "[_]");
+    Console.Write("Data: (dd/mm/aaaa)");
+    inputDate = Console.ReadLine();
+    date = DateTime.Parse(inputDate);
+
+    task = createTask(titleTask, taskText, "[_]", date);
     listTasks.AddRange(ListOfTasks(task));
 
     Console.WriteLine("Se voce nao deseja adicionar mais tarefa digite (sair): ");
@@ -93,6 +99,18 @@ do
     if (!_switch.Equals("sair", StringComparison.OrdinalIgnoreCase))
     {
         changeTask(listTasks, int.Parse(_switch));
+    }
+
+} while (!_switch.Equals("sair", StringComparison.OrdinalIgnoreCase));
+
+do
+{
+    Console.WriteLine("Digite o índice da tarefa para removê-la ou digite (sair) para sair: ");
+    _switch = Console.ReadLine();
+    // index = Convert.ToInt32(Console.ReadLine());
+    if (!_switch.Equals("sair", StringComparison.OrdinalIgnoreCase))
+    {
+        removeTaskOfList(listTasks, int.Parse(_switch));
     }
 
 } while (!_switch.Equals("sair", StringComparison.OrdinalIgnoreCase));
