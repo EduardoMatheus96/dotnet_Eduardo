134800684fecc4daf1ad486d77b54b61a9b82e7d
Eduardo Matheus De Menezes Souto <eduardomatheusuesc@gmail.com>
2023-11-29 09:33:23 -0300

    Terminando exercicio 01

---
+++ Semana04/exerciciosPoo/exercicio01/Program.cs
@@ -0,0 +1,12 @@
+﻿#region Manipulação classe veiculo
+
+Veiculo gol = new Veiculo();
+
+gol.Modelo = "Gol";
+gol.Ano = "1993";
+gol.Cor = "Verde-Limao";
+
+Console.WriteLine($"{gol.Modelo}, {gol.Ano}, {gol.Cor}");
+
+
+#endregion
---
+++ Semana04/exerciciosPoo/exercicio01/Veiculo.cs
@@ -0,0 +1,22 @@
+public class Veiculo
+{
+    public Veiculo() { }
+    string modelo;
+    string ano;
+    string cor;
+
+    public string Modelo
+    {
+        get { return modelo; }
+        set { modelo = value; }
+    }
+
+    public string Ano{
+        get { return ano; }
+        set { ano = value; }
+    }
+    public string Cor{
+        get { return cor; }
+        set { cor = value; }
+    }
+}
